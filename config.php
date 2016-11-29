481d31b8d23c734cedbbb79e2ede59c2c6087204
lilTylerD <lafootballfan96@yahoo.com>
2016-11-28 21:38:41 -0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// MySQL server credentials.
+$servername = 'localhost';
+$username = '';
+$password = '';
+$database = 'ruskin';
+$xml_folder = '../xml/';
+
+// Login credentials for the update tool.
+$updateToolPassword = '';
+?>
